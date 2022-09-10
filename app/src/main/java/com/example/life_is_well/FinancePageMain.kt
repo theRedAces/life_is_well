6b660148f4faa9c18146260df3806b041945c8c6
Shane Miller <SAMiller5@student.fullsail.edu>
2022-09-10 12:01:25 -0400

    Created layout for finance main page and implemented a back button to navigate back to home

--- app/src/main/java/com/example/life_is_well/FinancePageMain.kt
+++ app/src/main/java/com/example/life_is_well/FinancePageMain.kt
@@ -1,11 +1,25 @@
 package com.example.life_is_well
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FinancePageMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finance_page_main)
     }
+
+    fun financeBackBtn(view: View) {
+
+        val intent = Intent(this,MainActivity::class.java )
+        startActivity(intent)
+
+    }
+
+
+    fun accountBtnMain(view: View) {}
+    fun financeAddBtn(view: View) {}
+
 }
