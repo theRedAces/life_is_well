9f3d84d035bbb7fb02eda917365aba92f939cb87
Derek  Kensinger <dkensinger40@gmail.com>
2022-09-18 20:53:50 -0400

    Conflicts x2

--- app/src/main/java/com/example/life_is_well/HealthPageMain.kt
+++ app/src/main/java/com/example/life_is_well/HealthPageMain.kt
@@ -1,11 +1,29 @@
 package com.example.life_is_well
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 class HealthPageMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_health_page_main)
+        setContentView(R.layout.activity_main)
+
+        val macrosButton = findViewById<Button>(R.id.macroTrackerButton)
+        macrosButton.setOnClickListener {
+            val Intent = Intent(this, PhysicalMacroTracker::class.java)
+            startActivity(Intent)
+
+        }
     }
 }
+    /*fun healthBackBtn(view: View) {
+
+        val intent = Intent(this, MainActivity::class.java )
+        startActivity(intent)
+        }
+    */
+
