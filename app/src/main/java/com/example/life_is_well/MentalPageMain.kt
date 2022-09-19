5953af34c1515a33c054f323c15a43710d7e67ae
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-09-18 20:36:22 -0400

    adding mental features

--- app/src/main/java/com/example/life_is_well/MentalPageMain.kt
+++ app/src/main/java/com/example/life_is_well/MentalPageMain.kt
@@ -1,11 +1,44 @@
 package com.example.life_is_well
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.example.life_is_well.databinding.ActivityMentalPageMainBinding
 
 class MentalPageMain : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityMentalPageMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mental_page_main)
+
+        binding = ActivityMentalPageMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // setContentView(R.layout.activity_mental_page_main)
+
+       val meditateButton = findViewById<ImageButton>(R.id.imageButton6)
+        meditateButton.setOnClickListener {
+            val intent = Intent(this, MeditationMentalPage::class.java)
+            startActivity(intent)
+        }
+
+        val gratitudeJournalButton = findViewById<ImageButton>(R.id.imageButton7)
+        gratitudeJournalButton.setOnClickListener {
+            val intent = Intent(this, GratitudeMentalPage::class.java)
+            startActivity(intent)
+        }
+
+        val breathingExButton = findViewById<ImageButton>(R.id.imageButton8)
+        breathingExButton.setOnClickListener {
+            val intent = Intent(this, BreathingExerciseMentalPage::class.java)
+            startActivity(intent)
+        }
+
+        val toDoButton = findViewById<ImageButton>(R.id.imageButton9)
+       toDoButton.setOnClickListener {
+            val intent = Intent(this, ToDoMentalPage::class.java)
+            startActivity(intent)
+        }
     }
 }
