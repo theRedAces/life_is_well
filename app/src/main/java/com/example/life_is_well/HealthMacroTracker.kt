0801402a0a375cbf22ccc95844ad4f22a54af806
Derek  Kensinger <dkensinger40@gmail.com>
2022-09-20 20:34:09 -0400

    Please work!!

--- app/src/main/java/com/example/life_is_well/HealthMacroTracker.kt
+++ app/src/main/java/com/example/life_is_well/HealthMacroTracker.kt
@@ -52,9 +52,9 @@ class HealthMacroTracker : AppCompatActivity() {
             month,
             day,
         )
-      /*  dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
         dpd.show()
-    */
+
     }
 
 
