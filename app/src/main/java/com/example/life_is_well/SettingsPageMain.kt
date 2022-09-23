df63828ee16df2bc13cacd77546b26eab49f218c
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-09-23 18:06:27 -0400

    Updates

--- app/src/main/java/com/example/life_is_well/SettingsPageMain.kt
+++ app/src/main/java/com/example/life_is_well/SettingsPageMain.kt
@@ -20,7 +20,7 @@ class SettingsPageMain : AppCompatActivity() {
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            43ewsazxzsetPreferencesFromResource(R.xml.root_preferences, rootKey)
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
     }
 }
