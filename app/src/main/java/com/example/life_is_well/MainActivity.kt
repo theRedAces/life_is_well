26ed368ad03e7fab53e54b4e0a1f8aa9c6f9462b
Shane Miller <SAMiller5@student.fullsail.edu>
2022-10-02 17:59:11 -0400

    Changed Bot nav to intent method

--- app/src/main/java/com/example/life_is_well/MainActivity.kt
+++ app/src/main/java/com/example/life_is_well/MainActivity.kt
@@ -5,11 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.life_is_well.goals.GoalsPageMain
 
-
-//import fragments.SettingsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 
