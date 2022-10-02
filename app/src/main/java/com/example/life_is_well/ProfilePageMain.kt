49c71ae8989fbaabcf3585e6663a65fea9404136
Shane Miller <SAMiller5@student.fullsail.edu>
2022-10-02 17:42:13 -0400

    Changed Bot nav to intent method

---
+++ app/src/main/java/com/example/life_is_well/ProfilePageMain.kt
@@ -0,0 +1,11 @@
+package com.example.life_is_well
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilePageMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page_main)
+    }
+}
---
+++ app/src/main/java/com/example/life_is_well/goals/GoalsPageMain.kt
@@ -0,0 +1,12 @@
+package com.example.life_is_well.goals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.life_is_well.R
+
+class GoalsPageMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goals_page_main)
+    }
+}
