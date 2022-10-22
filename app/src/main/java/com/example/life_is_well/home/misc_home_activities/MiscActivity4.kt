cee8349b2de47ae1848f9c7f793fccd0ccc35c1a
Shane Miller <SAMiller5@student.fullsail.edu>
2022-10-22 19:28:46 -0400

    Button click functionality to the finance page, as well as hard coded basic layouts for the dynamic buttons to navigate to on the home and finance pages.

---
+++ app/src/main/java/com/example/life_is_well/home/misc_home_activities/MiscActivity4.kt
@@ -0,0 +1,12 @@
+package com.example.life_is_well.home.misc_home_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.life_is_well.R
+
+class MiscActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_misc4)
+    }
+}
