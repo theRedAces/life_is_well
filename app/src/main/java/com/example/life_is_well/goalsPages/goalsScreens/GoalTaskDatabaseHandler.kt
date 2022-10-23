ee014d68aa6ccbe45669825ccaf8ff30bc910875
Jacob Burgo <jacob.burgo@gleasontech.com>
2022-10-22 21:56:21 -0400

    Goal page works:

--- app/src/main/java/com/example/life_is_well/goalsPages/goalsScreens/GoalTaskDatabaseHandler.kt
+++ app/src/main/java/com/example/life_is_well/goalsPages/goalsScreens/GoalTaskDatabaseHandler.kt
@@ -1,4 +1,4 @@
-package com.example.life_is_well.goalsPages
+package com.example.life_is_well.goalsPages.goalsScreens
 
 import android.annotation.SuppressLint
 import android.content.ContentValues
@@ -11,25 +11,26 @@ import android.util.Log
 import com.example.life_is_well.R
 
 //creating the database logic, extending the SQLiteOpenHelper base class
-class GoalCardDatabaseHandler(context: Context) :
+class GoalTaskDatabaseHandler(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     companion object {
-        private const val DATABASE_VERSION = 2
-        private const val DATABASE_NAME = "GoalCardDatabase"
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "GoalTaskDatabase"
 
-        private const val TABLE_CONTACTS = "GoalsTable"
+        private const val TABLE_CONTACTS = "GoalTaskTable"
 
         private const val KEY_ID = "_id"
-        private const val KEY_TITLE = "goalTitle"
-        private const val KEY_DESCRIPTION = "goalDescription"
-        private const val KEY_ICON = "goalIcon"
+        private const val PARENT_KEY_ID = "_parent_id"
+        private const val KEY_TITLE = "taskTitle"
+        private const val KEY_DESCRIPTION = "taskDescription"
+        private const val KEY_ICON = "taskIcon"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         //creating table with fields
         val createContactsTable = ("CREATE TABLE " + TABLE_CONTACTS + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TITLE + " TEXT,"
+                + KEY_ID + " INTEGER PRIMARY KEY," + PARENT_KEY_ID + " INTEGER" + KEY_TITLE + " TEXT,"
                 + KEY_DESCRIPTION + " TEXT," + KEY_ICON + " TEXT" + ")")
         db?.execSQL(createContactsTable)
     }
@@ -42,20 +43,20 @@ class GoalCardDatabaseHandler(context: Context) :
     /**
      * Function to insert data
      */
-    fun addGoalCard(goal: GoalItem): Long {
+    fun addGoalTask(goal: GoalTaskItem): Long {
         val db = this.writableDatabase
 
         val contentValues = ContentValues()
-        contentValues.put(KEY_TITLE, goal.goalTitle)
-        contentValues.put(KEY_DESCRIPTION, goal.goalDescription)
+        contentValues.put(KEY_TITLE, goal.taskTitle)
+        contentValues.put(KEY_DESCRIPTION, goal.taskDescription)
 
-        if (goal.goalIcon == R.drawable.finance) {
+        if (goal.taskIcon == R.drawable.finance) {
             contentValues.put(KEY_ICON, "FINANCE")
-        } else if (goal.goalIcon == R.drawable.mental) {
+        } else if (goal.taskIcon == R.drawable.mental) {
             contentValues.put(KEY_ICON, "MENTAL")
-        } else if (goal.goalIcon == R.drawable.health) {
+        } else if (goal.taskIcon == R.drawable.health) {
             contentValues.put(KEY_ICON, "HEALTH")
-        } else if (goal.goalIcon == R.drawable.plus) {
+        } else if (goal.taskIcon == R.drawable.plus) {
             contentValues.put(KEY_ICON, "PLUS")
         }
 
@@ -69,9 +70,9 @@ class GoalCardDatabaseHandler(context: Context) :
 
     //Method to read the records from database in form of ArrayList
     @SuppressLint("Range")
-    fun viewGoalCards(): ArrayList<GoalItem> {
+    fun viewGoalCards(): ArrayList<GoalTaskItem> {
 
-        val goalList: ArrayList<GoalItem> = ArrayList()
+        val goalTaskList: ArrayList<GoalTaskItem> = ArrayList()
 
         // Query to select all the records from the table.
         val selectQuery = "SELECT  * FROM $TABLE_CONTACTS"
@@ -89,6 +90,7 @@ class GoalCardDatabaseHandler(context: Context) :
         }
 
         var id: Int
+        var parentId: Int
         var title: String
         var description: String
         var icon: String
@@ -97,6 +99,7 @@ class GoalCardDatabaseHandler(context: Context) :
         if (cursor.moveToFirst()) {
             do {
                 id = cursor.getInt(cursor.getColumnIndex(KEY_ID))
+                parentId = cursor.getInt(cursor.getColumnIndex(PARENT_KEY_ID))
                 title = cursor.getString(cursor.getColumnIndex(KEY_TITLE))
                 icon = cursor.getString(cursor.getColumnIndex(KEY_ICON))
                 description = cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION))
@@ -110,27 +113,28 @@ class GoalCardDatabaseHandler(context: Context) :
                 } else {
                     iconId = R.drawable.plus
                 }
-                val goal = GoalItem(goalId = id, goalTitle = title, goalDescription = description, goalIcon = iconId)
-                goalList.add(goal)
+                val task = GoalTaskItem(taskId = id, taskTitle = title, taskDescription = description, taskIcon = iconId, parentGoalId = parentId)
+                goalTaskList.add(task)
 
             } while (cursor.moveToNext())
         }
         cursor.close()
-        return goalList
+        return goalTaskList
     }
 
     /**
      * Function to update record
      */
-    fun updateGoalCard(goal: GoalItem): Int {
+    fun updateGoalCard(task: GoalTaskItem): Int {
         val db = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put(KEY_TITLE, goal.goalTitle)
-        contentValues.put(KEY_DESCRIPTION, goal.goalDescription)
-        contentValues.put(KEY_ICON, goal.goalIcon)
+        contentValues.put(KEY_TITLE, task.taskTitle)
+        contentValues.put(PARENT_KEY_ID, task.parentGoalId)
+        contentValues.put(KEY_DESCRIPTION, task.taskDescription)
+        contentValues.put(KEY_ICON, task.taskIcon)
 
         // Updating Row
-        val success = db.update(TABLE_CONTACTS, contentValues, KEY_ID + "=" + goal.goalId, null)
+        val success = db.update(TABLE_CONTACTS, contentValues, KEY_ID + "=" + task.taskId, null)
         //2nd argument is String containing nullColumnHack
 
         // Closing database connection
@@ -141,12 +145,12 @@ class GoalCardDatabaseHandler(context: Context) :
     /**
      * Function to delete record
      */
-    fun deleteGoalCard(goal: GoalItem): Int {
+    fun deleteGoalCard(task: GoalTaskItem): Int {
         val db = this.writableDatabase
         val contentValues = ContentValues()
-        contentValues.put(KEY_ID, goal.goalId) // EmpModelClass id
+        contentValues.put(KEY_ID, task.taskId) // EmpModelClass id
         // Deleting Row
-        val success = db.delete(TABLE_CONTACTS, KEY_ID + "=" + goal.goalId, null)
+        val success = db.delete(TABLE_CONTACTS, KEY_ID + "=" + task.taskId, null)
         //2nd argument is String containing nullColumnHack
 
         // Closing database connection
